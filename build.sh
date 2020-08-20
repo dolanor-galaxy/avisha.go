2307456b39ed06504840f96fb0257647e3061383
Jack Mordaunt <jackmordaunt@gmail.com>
2020-08-20 14:09:04 +0800

    feat: re-compile on file change

--- build.sh
+++ build.sh
@@ -5,12 +5,17 @@ function build {
 
 
 function run {
-    echo running ./cmd/$1
-    $(compile $1)
+    echo running ./cmd/$1 ${@:2}
+    $(compile $@)
 }
 
 function compile {
     mkdir -p ./target/$1
     go build -o ./target/$1/avisha ./cmd/$1
-    echo target/$1/avisha
+    echo target/$1/avisha ${@:2}
+}
+
+function watch {
+    # https://github.com/watchexec/watchexec
+    watchexec -r "go build -o ./target/$1/avisha ./cmd/$1 && ./target/$1/avisha"
 }
