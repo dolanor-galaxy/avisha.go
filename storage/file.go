b44ae7ed28fb5dd7a6b18003d97ce9baae1b2d6e
Jack Mordaunt <jackmordaunt@gmail.com>
2020-08-25 10:38:51 +0800

    feat: stabled ordered iteration of data

--- storage/file.go
+++ storage/file.go
@@ -19,16 +19,32 @@ type File struct {
 	Path string
 	// Runtime type information.
 	Types map[string]Entity
+	// Data to serialize.
+	Data
+	// format the json output.
+	format bool
+}
+
+// Data to serialize.
+type Data struct {
 	// One bucket per type: "[type-name][entity-id]value".
 	Buckets map[string]map[string]json.RawMessage
+	// Order to iterate buckets.
+	BucketOrder []string
+	// Order to iterate bucket values.
+	ValueOrder map[string][]string
 }
 
 // FileStorage allocates a File storage solution.
 func FileStorage(path string) *File {
 	f := &File{
-		Path:    path,
-		Types:   make(map[string]Entity),
-		Buckets: make(map[string]map[string]json.RawMessage),
+		Path:  path,
+		Types: make(map[string]Entity),
+		Data: Data{
+			Buckets:     make(map[string]map[string]json.RawMessage),
+			BucketOrder: []string{},
+			ValueOrder:  make(map[string][]string),
+		},
 	}
 	return f
 }
@@ -43,6 +59,13 @@ func (f *File) With(prototype Entity) *File {
 	}
 	f.Types[name] = prototype
 	f.Buckets[name] = make(map[string]json.RawMessage)
+	f.BucketOrder = append(f.BucketOrder, name)
+	return f
+}
+
+// Format json output.
+func (f *File) Format(format bool) *File {
+	f.format = format
 	return f
 }
 
@@ -57,7 +80,8 @@ func (f *File) MustLoad() *File {
 // Query the file storage.
 func (f *File) Query(plist ...func(Entity) bool) (Entity, bool) {
 	// Loop over all the buckets, every entity will be tested.
-	for T, bucket := range f.Buckets {
+	for _, T := range f.BucketOrder {
+		bucket := f.Buckets[T]
 		for _, by := range bucket {
 			// Deserialize each entity using the appropriate type.
 			ent, ok := reflect.New(reflect.ValueOf(f.Types[T]).Elem().Type()).Interface().(Entity)
@@ -79,8 +103,10 @@ func (f *File) Query(plist ...func(Entity) bool) (Entity, bool) {
 func (f *File) List(plist ...func(Entity) bool) []Entity {
 	var found []Entity
 	// Loop over all the buckets, every entity will be tested.
-	for T, bucket := range f.Buckets {
-		for _, by := range bucket {
+	for _, T := range f.BucketOrder {
+		bucket := f.Buckets[T]
+		for _, order := range f.ValueOrder[T] {
+			by := bucket[order]
 			// Deserialize each entity using the appropriate type.
 			ent, ok := reflect.New(reflect.ValueOf(f.Types[T]).Elem().Type()).Interface().(Entity)
 			if !ok {
@@ -111,11 +137,12 @@ func (f *File) Update(ent Entity) error {
 func (f *File) Save(ent Entity) error {
 	for T := range f.Types {
 		if T == reflect.TypeOf(ent).Name() {
-			v, err := json.MarshalIndent(ent, "", "\t")
+			v, err := json.Marshal(ent)
 			if err != nil {
 				return fmt.Errorf("serializing entity: %w", err)
 			}
 			f.Buckets[T][ent.ID()] = v
+			f.ValueOrder[T] = append(f.ValueOrder[T], ent.ID())
 			break
 		}
 	}
@@ -134,19 +161,31 @@ func (f *File) Delete(ent Entity) error {
 }
 
 func (f *File) save() error {
+	var (
+		indent = ""
+	)
+
 	os.Remove(f.Path) // Overwrite existing.
+
 	handle, err := f.prepare()
 	if err != nil {
 		return err
 	}
 	defer handle.Close()
-	by, err := json.Marshal(f.Buckets)
+
+	if f.format {
+		indent = "\t"
+	}
+
+	by, err := json.MarshalIndent(f.Data, "", indent)
 	if err != nil {
 		return fmt.Errorf("serializing buckets: %w", err)
 	}
+
 	if _, err := io.Copy(handle, bytes.NewBuffer(by)); err != nil {
 		return fmt.Errorf("writing file: %w", err)
 	}
+
 	return nil
 }
 
@@ -163,7 +202,7 @@ func (f *File) load() error {
 	if len(by) == 0 {
 		return nil
 	}
-	if err := json.Unmarshal(by, &f.Buckets); err != nil {
+	if err := json.Unmarshal(by, &f.Data); err != nil {
 		return fmt.Errorf("deserializing buckets: %w", err)
 	}
 	return nil
