fc573f96d2ae92c5a1ba282090bc4d5ddcdcba10
Jack Mordaunt <jackmordaunt@gmail.com>
2020-12-26 14:01:00 +0800

    fix: was treating cents as dollars
    Signed-off-by: Jack Mordaunt <jackmordaunt@gmail.com>

--- currency/currency.go
+++ currency/currency.go
@@ -17,16 +17,8 @@ func (c Currency) Mills() int64 {
 	return int64(c)
 }
 
-func (c Currency) Cents() int64 {
-	return int64(c) / 10
-}
-
 func (c Currency) Dollars() float64 {
-	var (
-		dollars = c / Dollar
-		cents   = (c % Dollar) / Cent
-	)
-	return float64(dollars) + float64(cents)
+	return float64(c) / float64(Dollar)
 }
 
 func (c Currency) String() string {
